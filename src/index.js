9fcc01e030c10bda7686df44374ec62b80b30366
Niko464 <58821733+Niko464@users.noreply.github.com>
2023-01-20 02:19:53 +0100

    Retesting deployment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-import { createRoot } from "react-dom/client";
 import "./styles/global.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ReactDOM from "react-dom";
 
-const root = createRoot(document.getElementById("root"));
-
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
-  </BrowserRouter>
-);
+ReactDOM.render(<HomePage />, document.getElementById('root'));
